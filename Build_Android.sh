307fa576c0c0104154c3a35b845a8ecfcc694af3
yanzhubiao <yanzhubiao@163.com>
2018-12-14 17:07:03 +0800

    Reformatting shell provisioners (#3)

--- Build_Android.sh
+++ Build_Android.sh
@@ -1,46 +1,44 @@
 #!/bin/bash
 
-function changesource {
-    sed -i 's:define LUA_IDSIZE:define LUA_IDSIZE    256  // :' luaconf.h
+if [[ -z "${LUA_UE4_VERSION}" ]]; then
+  echo "LUA_UE4_VERSION is not set, exit."
+  exit 1
+else
+  echo "LUA_UE4_VERSION: ${LUA_UE4_VERSION}"
+fi
+
+if [[ -z "${LUA_UE4_PREFIX}" ]]; then
+  echo "LUA_UE4_PREFIX is not set, exit."
+  exit 1
+else
+  echo "LUA_UE4_PREFIX: ${LUA_UE4_PREFIX}"
+fi
+
+readonly LUA_UE4_URL=http://www.lua.org/ftp/lua-${LUA_UE4_VERSION}.tar.gz
+readonly LUA_UE4_DIR=lua-${LUA_UE4_VERSION}
+readonly LUA_UE4_TAR=${LUA_UE4_DIR}.tar.gz
+
+function change_source {
+  sed -i 's:define LUA_IDSIZE:define LUA_IDSIZE    256  // :' luaconf.h
 }
 
-if [ -z "$LUA_UE4_VERSION" ]; then
-    echo "LUA_UE4_VERSION is not set, exit."
-    exit 1
-else
-    echo "LUA_UE4_VERSION: $LUA_UE4_VERSION"
-fi
-
-if [ -z "$LUA_UE4_PREFIX" ]; then
-    echo "LUA_UE4_PREFIX is not set, exit."
-    exit 1
-else
-    echo "LUA_UE4_PREFIX: $LUA_UE4_PREFIX"
-fi
-
-CUR_DIR=$(dirname $0)
-if [ "${CUR_DIR}" = "." ]; then
-    CUR_DIR=`pwd`
-fi
-
-LUA_UE4_URL=http://www.lua.org/ftp/lua-${LUA_UE4_VERSION}.tar.gz
-LUA_UE4_DIR=lua-$LUA_UE4_VERSION
-LUA_UE4_TAR=$LUA_UE4_DIR.tar.gz
-
-wget -q -O $LUA_UE4_TAR $LUA_UE4_URL
-
-tar zxf $LUA_UE4_TAR
-mv ./$LUA_UE4_DIR/* .
-rm -rf $LUA_UE4_DIR
+wget -q -O ${LUA_UE4_TAR} ${LUA_UE4_URL}
+tar zxf ${LUA_UE4_TAR}
+mv ./${LUA_UE4_DIR}/* .
+rm -rf ${LUA_UE4_DIR}
 
 cd src
-changesource
-
+change_source
 cd ..
 
-rm -rf $LUA_UE4_PREFIX
-mkdir -p $LUA_UE4_PREFIX
+rm -rf ${LUA_UE4_PREFIX}
+mkdir -p ${LUA_UE4_PREFIX}
 
-cmake -DCMAKE_INSTALL_PREFIX=$LUA_UE4_PREFIX/android . -DCMAKE_TOOLCHAIN_FILE=android.toolchain.cmake -DANDROID_NDK="${NDKROOT}" -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a"
+cmake .                                                \
+  -DCMAKE_INSTALL_PREFIX=${LUA_UE4_PREFIX}/android     \
+  -DCMAKE_TOOLCHAIN_FILE=android.toolchain.cmake       \
+  -DANDROID_NDK="${NDKROOT}"                           \
+  -DCMAKE_BUILD_TYPE=Release                           \
+  -DANDROID_ABI="armeabi-v7a"
 make
 make install
--- Build_IOS.sh
+++ Build_IOS.sh
@@ -1,47 +1,41 @@
 #!/bin/bash
 
-CUR_DIR=$(dirname $0)
-if [ "${CUR_DIR}" = "." ]; then
-    CUR_DIR=`pwd`
+if [[ -z "${LUA_UE4_VERSION}" ]]; then
+  echo "LUA_UE4_VERSION is not set, exit."
+  exit 1
+else
+  echo "LUA_UE4_VERSION: ${LUA_UE4_VERSION}"
 fi
 
-CORE_COUNT=$(sysctl -n machdep.cpu.core_count)
+if [[ -z "${LUA_UE4_PREFIX}" ]]; then
+  echo "LUA_UE4_PREFIX is not set, exit."
+  exit 1
+else
+  echo "LUA_UE4_PREFIX: ${LUA_UE4_PREFIX}"
+fi
 
-changesource(){
-    sed -i "" 's:define LUA_IDSIZE:define LUA_IDSIZE    256  // :' luaconf.h
-    ADD_NUMBER_LINE=$(sed -n -e "/without modifying the main part of the file./=" luaconf.h)
-    if [ -n ${ADD_NUMBER_LINE} ]; then
-        ADD_NUMBER_LINE=$(( ${ADD_NUMBER_LINE} + 2 ))
-        sed -i '' "${ADD_NUMBER_LINE} a\ 
-        #define system(s) ((s)==NULL ? 0 : -1)
-        " luaconf.h
-    fi
+if [[ -z "${LUA_UE4_IOS_DEPLOYMENT_TARGET}" ]]; then
+  echo "LUA_UE4_IOS_DEPLOYMENT_TARGET is not set, exit."
+  exit 1
+else
+  echo "LUA_UE4_IOS_DEPLOYMENT_TARGET: ${LUA_UE4_IOS_DEPLOYMENT_TARGET}"
+fi
+
+readonly CORE_COUNT=$(sysctl -n machdep.cpu.core_count)
+readonly LUA_UE4_URL=http://www.lua.org/ftp/lua-${LUA_UE4_VERSION}.tar.gz
+readonly LUA_UE4_DIR=lua-${LUA_UE4_VERSION}
+readonly LUA_UE4_TAR=${LUA_UE4_DIR}.tar.gz
+
+function change_source {
+  sed -i "" 's:define LUA_IDSIZE:define LUA_IDSIZE    256  // :' luaconf.h
+  ADD_NUMBER_LINE=$(sed -n -e "/without modifying the main part of the file./=" luaconf.h)
+  if [[ -n ${ADD_NUMBER_LINE} ]]; then
+    ADD_NUMBER_LINE=$((${ADD_NUMBER_LINE} + 2))
+    sed -i '' "${ADD_NUMBER_LINE} a\ 
+    #define system(s) ((s)==NULL ? 0 : -1)
+    " luaconf.h
+  fi
 }
-
-if [ -z "${LUA_UE4_VERSION}" ]; then
-    echo "LUA_UE4_VERSION is not set, exit."
-    exit 1
-else
-    echo "LUA_UE4_VERSION: $LUA_UE4_VERSION"
-fi
-
-if [ -z "${LUA_UE4_PREFIX}" ]; then
-    echo "LUA_UE4_PREFIX is not set, exit."
-    exit 1
-else
-    echo "LUA_UE4_PREFIX: $LUA_UE4_PREFIX"
-fi
-
-if [ -z "${LUA_UE4_IOS_DEPLOYMENT_TARGET}" ]; then
-    echo "LUA_UE4_IOS_DEPLOYMENT_TARGET is not set, exit."
-    exit 1
-else
-    echo "LUA_UE4_IOS_DEPLOYMENT_TARGET: $LUA_UE4_IOS_DEPLOYMENT_TARGET"
-fi
-
-LUA_UE4_URL=http://www.lua.org/ftp/lua-${LUA_UE4_VERSION}.tar.gz
-LUA_UE4_DIR=lua-${LUA_UE4_VERSION}
-LUA_UE4_TAR=${LUA_UE4_DIR}.tar.gz
 
 wget -q -O ${LUA_UE4_TAR} ${LUA_UE4_URL}
 tar zxf ${LUA_UE4_TAR}
@@ -49,22 +43,20 @@ mv ./${LUA_UE4_DIR}/* .
 rm -rf ${LUA_UE4_DIR}
 
 cd src
-changesource
+change_source
 cd ..
 
-rm -rf $LUA_UE4_PREFIX
-mkdir -p $LUA_UE4_PREFIX
-
+rm -rf ${LUA_UE4_PREFIX}
+mkdir -p ${LUA_UE4_PREFIX}
 mkdir -p ${LUA_UE4_PREFIX}/IOS/lib
 
-cmake -DCMAKE_INSTALL_PREFIX=$LUA_UE4_PREFIX/IOS . -G "Xcode"
-
+cmake -DCMAKE_INSTALL_PREFIX=${LUA_UE4_PREFIX}/IOS . -G "Xcode"
 xcodebuild -project "lua.xcodeproj"                             \
   -target "lua_static"                                          \
   -configuration Release                                        \
   -sdk iphoneos                                                 \
   -arch arm64                                                   \
-  IPHONEOS_DEPLOYMENT_TARGET=$LUA_UE4_IOS_DEPLOYMENT_TARGET     \
+  IPHONEOS_DEPLOYMENT_TARGET=${LUA_UE4_IOS_DEPLOYMENT_TARGET}   \
   -jobs ${CORE_COUNT}                                           \
   build
 xcodebuild -target install build
